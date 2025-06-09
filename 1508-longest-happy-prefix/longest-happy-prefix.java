09f43a4639ebc3384ff0bf64b525b6326baa5dff
Dipanta <116421879+Wodenvase@users.noreply.github.com>
2025-06-09 11:45:50 +0530

    Time: 11 ms (50.18%) | Memory: 45.5 MB (66.90%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.java
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -1,14 +1,22 @@
 class Solution {
     public String longestPrefix(String s) {
-        String res = "";
-        for (int i = s.length()-1; i>=0; i--){
-            String str = s.substring(0, i);
-            if (s.endsWith(str)){
-                res+=str;
-                break;
+        int[] dp = new int[s.length()];
+        int len = 0, i = 1;
+        while(i < s.length()){
+            if(s.charAt(i) == s.charAt(len)){
+                dp[i] = ++len;
+                i++;
+            }
+            else{
+                if(len>0){
+                    len = dp[len-1];
+                }
+                else{
+                    dp[i] = 0;
+                    i++;
+                }
             }
         }
-        return res;
-        
+        return s.substring(0,dp[dp.length-1]);
     }
 }
